254b2bad9d8a893a86d67cce3997b6219f38cdc3
yerragollabhargavi <150415432+yerragollabhargavi@users.noreply.github.com>
2024-09-11 21:31:58 +0530

    Add files via upload

--- StockPortFolio.py
+++ StockPortFolio.py
@@ -1,22 +1,28 @@
 import requests
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 API_KEY="7VKA3JPLU70ZMEUP"
 portfolio={}
 def add_stock(symbol,shares):
+    shares=int(shares)
+    symbol=symbol.upper()
     portfolio[symbol]=portfolio.get(symbol,0)+shares
     print(f"Added {shares} shares of {symbol} to the portfolio")
-def remove_stock(symbol,shares):
+
+def remove_stock(symbol, shares):
+    """Remove a stock from the portfolio."""
+    shares = int(shares)
+    symbol = symbol.upper()  
     if symbol in portfolio:
-        if portfolio[symbol]>=shares:
-            portfolio[symbol]-=shares
-            if portfolio[symbol]==0:
+        if portfolio[symbol] >= shares:
+            portfolio[symbol] -= shares
+            if portfolio[symbol] == 0:
                 del portfolio[symbol]
-            print(f"Removed {shares} shares of {symbol} from the portfolio")
+            print(f"Removed {shares} shares of {symbol} from the portfolio.")
         else:
-            print(f"Cannot remove {shares} shares of {symbol} as you only own {portfolio[symbol]} shares")
+            print(f"Cannot remove {shares} shares of {symbol} as you only own {portfolio[symbol]} shares.")
     else:
-        print(f"{symbol} is not found in the portfolio")
+        print(f"{symbol} is not found in the portfolio.")
 def get_stock_data(symbol):
     url=f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=1min&apikey={API_KEY}"
     response=requests.get(url)
@@ -26,7 +32,7 @@ def get_stock_data(symbol):
 def get_stock_price(symbol):
     data=get_stock_data(symbol)
     if 'Time Series (1min)' in data:
-        latest_time=sorted(data['Time Series (1min)'].keys())[0]
+        latest_time=sorted(data['Time Series (1min)'].keys())[-1]
         return float(data['Time Series (1min)'][latest_time]['4. close'])
     else:
         print(f"Failed to retrived data for {symbol}")
@@ -40,7 +46,12 @@ def track_portfolio():
             print(f"{symbol}:{shares} shares at ${price} each.")
     print(f"Total portfolio value: ${total_value}")
 
-add_stock('AAPL',10)
-remove_stock('AAPL',5)
-remove_stock('AAPL',10)
+
+action=input("Enter the action add/remove Stocks : ").strip()
+stock=input("Enter the Symbol and Shares : ").split()
+symbol,shares=stock[0],stock[1]
+if action=='add':
+    add_stock(symbol,shares)  
+else:
+    remove_stock(symbol,shares)
 track_portfolio()
